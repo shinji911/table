c0fab794c50d20ba4ed2129acf3da828766452f4
kshinji911 <kshinji911@gmail.com>
2019-02-16 11:16:59 -0500

    api1

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Tables data
-var tables = [
+let tables = [
     {
         TableNo: 1,
         ID: 333,
@@ -24,6 +24,21 @@ var tables = [
     {
         TableNo: 2,
         ID: 334,
+        name: "Vegeta",
+        email: "iamthestrongest@saiyan.com",
+        phone: "789-789-1000"
+    }
+];
+
+let waitlist = [
+    {
+        lineNo: 1,
+        name: "Yoda",
+        email: "greenturd@jedimasters.org",
+        phone: "456-123-7894"
+    },
+    {
+        lineNo: 2,
         name: "Vegeta",
         email: "iamthestrongest@saiyan.com",
         phone: "789-789-1000"
@@ -50,22 +65,19 @@ app.get("/api/tables", function (req, res) {
     return res.json(tables);
 });
 
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
+
 
 // Create New Reservations - takes in JSON input
 app.post("/reserve", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newcharacter = req.body;
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    var newReserve = req.body;    
 
-    console.log(newcharacter);
+    tables.push(newReserve);
 
-    characters.push(newcharacter);
-
-    res.json(newcharacter);
+    res.json();
 });
 
 // Starts the server to begin listening
