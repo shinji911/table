da93b7d823278543f0af021d7f2b4f6d68401334
kshinji911 <kshinji911@gmail.com>
2019-02-16 11:04:36 -0500

    api

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Tables data
+var tables = [
+    {
+        TableNo: 1,
+        ID: 333,
+        name: "Yoda",
+        email: "greenturd@jedimasters.org",
+        phone: "456-123-7894"
+    },
+    {
+        TableNo: 2,
+        ID: 334,
+        name: "Vegeta",
+        email: "iamthestrongest@saiyan.com",
+        phone: "789-789-1000"
+    }
+];
 
 // Routes
 // =============================================================
@@ -28,6 +45,10 @@ app.get("/reserve", function (req, res) {
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 })
+
+app.get("/api/tables", function (req, res) {
+    return res.json(tables);
+});
 
 
 // Create New Reservations - takes in JSON input
